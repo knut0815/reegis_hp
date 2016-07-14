81db1f7d4e8a253e87a19bf0deccbafbce7dda7b
uwe <uwe.krien@rl-institut.de>
2016-07-14 10:40:41 +0200

    adapt main file to new solph

--- reegis-hp/berlin_hp/main.py
+++ reegis-hp/berlin_hp/main.py
@@ -5,21 +5,21 @@ Created on Mon Apr 18 13:42:14 2016
 @author: uwe
 """
 
-import pandas as pd
-import logging
-
-# import solph module to create/process optimization model instance
-from oemof.solph import predefined_objectives as predefined_objectives
+# Outputlib
+from oemof.outputlib import to_pandas as tpd
 
 # Default logger of oemof
 from oemof.tools import logger
 
 # import oemof base classes to create energy system objects
-from oemof.core import energy_system as es
-from oemof.core.network.entities import Bus as bus
-from oemof.core.network.entities.components import sinks as sink
-from oemof.core.network.entities.components import sources as source
-from oemof.core.network.entities.components import transformers as transformer
+import logging
+import pandas as pd
+import matplotlib.pyplot as plt
+from oemof.core import energy_system as core_es
+import oemof.solph as solph
+from oemof.solph import (Bus, Source, Sink, Flow, LinearTransformer, Storage)
+from oemof.solph.network import Investment
+from oemof.solph import OperationalModel
 
 
 # Define logger
@@ -38,33 +38,31 @@ berlin_e_system = es.EnergySystem(time_idx=time_index, simulation=simulation)
 
 logging.info("Adding objects to the energy system...")
 
-es_dict = {}
-
 # Create buses
-es_dict['bus'] = {}
+es_dict = {'bus': {}}
 
 # Electricity
-es_dict['bus']['el'] = bus(uid='bel', type='el')
+es_dict['bus']['el'] = bel = Bus(label="electricity")
 
 # Natural gas
-es_dict['bus']['ngas'] = bus(uid='bngas', type='ngas')
+es_dict['bus']['ngas'] = Bus(label="natural_gas")
 
 # Oil
-es_dict['bus']['oil'] = bus(uid='boil', type='oil')
+es_dict['bus']['oil'] = Bus(label='oil')
 
 # Coal
-es_dict['bus']['coal'] = bus(uid='bcoaloil', type='coal')
+es_dict['bus']['coal'] = Bus(label='coal')
 
 # Lignite
-es_dict['bus']['lignite'] = bus(uid='blignite', type='lignite')
+es_dict['bus']['lignite'] = Bus(label='lignite')
 
 # Biomass
-es_dict['bus']['biom'] = bus(uid='bbiom', type='biomass')
+es_dict['bus']['biom'] = Bus(label='biomass')
 
 # Create district heat buses
 for n in range(number_of_district_heating_systems):
-    uid = 'distr' + str(n + 1)
-    es_dict['bus'][uid] = bus(uid='b' + uid, type='heat')
+    label_string = 'distr' + str(n + 1)
+    es_dict['bus'][label_string] = Bus(label=label_string)
 
 # Create sources
 
