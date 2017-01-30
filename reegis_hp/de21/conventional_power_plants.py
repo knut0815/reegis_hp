21d6b7813187375bdb49a0540f005be6fbaf3632
uwe <uwe.krien@rl-institut.de>
2017-01-30 12:05:59 +0100

    extend example

--- reegis_hp/de21/conventional_power_plants.py
+++ reegis_hp/de21/conventional_power_plants.py
@@ -6,6 +6,9 @@ del cpp['Unnamed: 0']
 print(cpp.columns)
 
 cpp_by_region_fuel = cpp.groupby(['region', 'fuel'])
-print(cpp_by_region_fuel[['capacity_net_bnetza',  'capacity_gross_uba']].sum())
-print(cpp_by_region_fuel[['efficiency_data',  'efficiency_source',
-                          'efficiency_estimate']].mean())
+
+capacity_by_region_fuel = cpp_by_region_fuel[['capacity_net_bnetza',
+                                              'capacity_gross_uba']].sum()
+
+print(capacity_by_region_fuel)
+print(cpp_by_region_fuel[['efficiency_data', 'efficiency_estimate']].mean())
