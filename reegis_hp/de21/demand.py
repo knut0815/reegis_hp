040bcc1b534613cad972c7addce2f0864bc68bf7
uwe <uwe.krien@rl-institut.de>
2017-02-06 11:50:56 +0100

    add demand time series

---
+++ reegis_hp/de21/demand.py
@@ -0,0 +1,67 @@
+# http://data.open-power-system-data.org/time_series/2016-10-28/time_series_60min_singleindex.csv
+
+import os
+import logging
+import requests
+import pandas as pd
+import datetime
+from matplotlib import pyplot as plt
+
+FIXED = False
+FILEPATH = os.path.join('data', 'entsoe_DE_load.csv')
+
+def read_original_file():
+    """Read file if exists."""
+
+    orig_csv_file = os.path.join('data_original',
+                                 'time_series_60min_singleindex.csv')
+    info_file = os.path.join('data_basic',
+                             'time_series_60min_singleindex.info.csv')
+
+    if not os.path.isdir('data_original'):
+        os.makedirs('data_original')
+
+    if not os.path.isfile(orig_csv_file):
+        csv = pd.read_csv(info_file, squeeze=True, index_col=[0])
+        req = requests.get(csv.download)
+        logging.warning("File not found. Try to download it from server.")
+        logging.warning("Check URL if download does not work.")
+        with open(orig_csv_file, 'wb') as fout:
+            fout.write(req.content)
+        logging.warning("Downloaded from {0} and copied to '{1}'.".format(
+            csv.download, orig_csv_file))
+        logging.warning("This script is tested with the file of {0}.".format(
+            csv.date))
+
+    return pd.read_csv(orig_csv_file)
+
+
+def prepare_demand_file():
+    """Convert demand file. CET index and Germany's load only."""
+    load = read_original_file()
+
+    load['cet'] = (
+        pd.to_datetime(load.cet_cest_timestamp) + datetime.timedelta(hours=1))
+
+    load = load.set_index('cet')
+
+    load[['utc_timestamp', 'cet_cest_timestamp', 'comment', 'load_DE_load']
+         ].to_csv(FILEPATH)
+
+
+def get_time_period(start, end):
+    load = pd.read_csv(FILEPATH, index_col='cet',
+                  parse_dates=True)
+    return load.ix[start:end]
+
+
+if not os.path.isfile(FILEPATH):
+    prepare_demand_file()
+
+start = datetime.datetime(2014, 1, 1, 0, 0)
+end = datetime.datetime(2014, 12, 31, 23, 0)
+
+load_profile = get_time_period(start, end)
+load_profile.plot()
+plt.show()
+print(load_profile.sum())
