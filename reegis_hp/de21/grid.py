217e2c4d1006838b486b78752efd4afe060a5999
uwe <uwe.krien@rl-institut.de>
2017-02-06 18:38:24 +0100

    use grid map to find available connections

--- reegis_hp/de21/grid.py
+++ reegis_hp/de21/grid.py
@@ -7,25 +7,41 @@ grid = pd.read_csv(os.path.join('data_basic', 'de21_grid_data.csv'))
 f_security = 0.7
 current_max = 2720
 
+
+def get_grid_capacity(plus, minus):
+    tmp_grid = grid.query("plus_region_id == {:0d} & ".format(plus) +
+                          "minus_region_id == {:1d} & ".format(minus) +
+                          "scenario_name == 'status_quo_2012_distance'")
+
+    line_id = 'DE{:02.0f}_DE{:02.0f}'.format(minus - 11000, plus - 11000)
+    de_grid.loc[line_id, 'id_A'] = 'DE{:02.0f}'.format(minus - 11000)
+    de_grid.loc[line_id, 'id_B'] = 'DE{:02.0f}'.format(plus - 11000)
+    if len(tmp_grid) > 0:
+        de_grid.loc[line_id, 'capacity_MW'] = tmp_grid.capacity_calc.sum()
+        de_grid.loc[line_id, 'distance_km'] = tmp_grid.distance.iloc[0]
+    else:
+        de_grid.loc[line_id, 'capacity_MW'] = 0
+        de_grid.loc[line_id, 'distance_km'] = 0
+
+
 # renpass F.Wiese (page 49)
 grid['capacity_calc'] = (grid.circuits * current_max * grid.voltage *
                          f_security * math.sqrt(3) / 1000)
 
 de_grid = pd.DataFrame()
 
-for plus in range(11001, 11022):
-    for minus in range(11001, 11022):
-        tmp_grid = grid.query("plus_region_id == {:0d} & ".format(plus) +
-                              "minus_region_id == {:1d} & ".format(minus) +
-                              "scenario_name == 'status_quo_2012_distance'")
-        if len(tmp_grid) > 0:
-            line_id = 'DE{:02.0f}_DE{:02.0f}'.format(minus - 11000,
-                                                     plus - 11000)
+lines = pd.read_csv(os.path.join('geometries', 'lines_de21.csv'),
+                    index_col='name')
 
-            de_grid.loc[line_id, 'id_A'] = 'DE{:02.0f}'.format(minus - 11000)
-            de_grid.loc[line_id, 'id_B'] = 'DE{:02.0f}'.format(plus - 11000)
-            de_grid.loc[line_id, 'capacity_MW'] = tmp_grid.capacity_calc.sum()
-            de_grid.loc[line_id, 'distance_km'] = tmp_grid.distance.iloc[0]
+for l in lines.index:
+    split = l.split('-')
+    A = ('110{0}'.format(split[0][2:]))
+    B = ('110{0}'.format(split[1][2:]))
+    print(A, B)
+    get_grid_capacity(int(A), int(B))
+    get_grid_capacity(int(B), int(A))
+
+# Behalte nur eine Richtung. Wenn eins null dann Wert behalten, sonst egal.
 
 print(de_grid)
 de_grid.to_csv(os.path.join('data', 'de_grid.csv'))
