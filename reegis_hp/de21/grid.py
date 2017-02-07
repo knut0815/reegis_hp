77e4642c6b7829c5ae59734c6e7690611d369d9e
uvchik <uwe.krien@rl-institut.de>
2017-02-07 13:51:05 +0100

    improve grid calculations and plotting

--- reegis_hp/de21/grid.py
+++ reegis_hp/de21/grid.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import math
+import geoplot
+from matplotlib import pyplot as plt
+
 
 grid = pd.read_csv(os.path.join('data_basic', 'de21_grid_data.csv'))
 
@@ -9,19 +12,22 @@ current_max = 2720
 
 
 def get_grid_capacity(plus, minus):
+    line_exists = False
     tmp_grid = grid.query("plus_region_id == {:0d} & ".format(plus) +
                           "minus_region_id == {:1d} & ".format(minus) +
                           "scenario_name == 'status_quo_2012_distance'")
 
-    line_id = 'DE{:02.0f}_DE{:02.0f}'.format(minus - 11000, plus - 11000)
+    line_id = 'DE{:02.0f}-DE{:02.0f}'.format(minus - 11000, plus - 11000)
     de_grid.loc[line_id, 'id_A'] = 'DE{:02.0f}'.format(minus - 11000)
     de_grid.loc[line_id, 'id_B'] = 'DE{:02.0f}'.format(plus - 11000)
     if len(tmp_grid) > 0:
+        line_exists = True
         de_grid.loc[line_id, 'capacity_MW'] = tmp_grid.capacity_calc.sum()
         de_grid.loc[line_id, 'distance_km'] = tmp_grid.distance.iloc[0]
     else:
         de_grid.loc[line_id, 'capacity_MW'] = 0
         de_grid.loc[line_id, 'distance_km'] = 0
+    return line_exists
 
 
 # renpass F.Wiese (page 49)
@@ -35,13 +41,34 @@ lines = pd.read_csv(os.path.join('geometries', 'lines_de21.csv'),
 
 for l in lines.index:
     split = l.split('-')
-    A = ('110{0}'.format(split[0][2:]))
-    B = ('110{0}'.format(split[1][2:]))
-    print(A, B)
-    get_grid_capacity(int(A), int(B))
-    get_grid_capacity(int(B), int(A))
+    a = ('110{0}'.format(split[0][2:]))
+    b = ('110{0}'.format(split[1][2:]))
+    # print(a, b)
+    a_b_exists = get_grid_capacity(int(a), int(b))
+    b_a_exists = get_grid_capacity(int(b), int(a))
 
-# Behalte nur eine Richtung. Wenn eins null dann Wert behalten, sonst egal.
+    line_id_a = 'DE{0}-DE{1}'.format(split[0][2:], split[1][2:])
+    line_id_b = 'DE{0}-DE{1}'.format(split[1][2:], split[0][2:])
 
-print(de_grid)
+    if not a_b_exists and not b_a_exists:
+        de_grid = de_grid[de_grid.index != line_id_a]
+    elif a_b_exists and not b_a_exists:
+        de_grid = de_grid[de_grid.index != line_id_a]
+    elif not a_b_exists and b_a_exists:
+        de_grid = de_grid[de_grid.index != line_id_b]
+    else:
+        print("Error in {0}".format(l))
+
+# print(de_grid)
+
+background = pd.read_csv('geometries/polygons_de21_simple.csv', index_col='gid')
+poly = geoplot.postgis2shapely(background.geom)
+plotter = geoplot.GeoPlotter(poly, (3, 16, 47, 56))
+plt.show()
+
 de_grid.to_csv(os.path.join('data', 'de_grid.csv'))
+for l in lines.index:
+    print(l)
+
+
+
