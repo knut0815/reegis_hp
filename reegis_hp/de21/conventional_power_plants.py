4bc40ea532eae017174576dcb1bec0d5cf28eb11
uwe <uwe.krien@rl-institut.de>
2017-02-22 13:30:59 +0100

    update example code

--- reegis_hp/de21/conventional_power_plants.py
+++ reegis_hp/de21/conventional_power_plants.py
@@ -3,12 +3,20 @@ import pandas as pd
 cpp = pd.read_csv('data/conv_power_plants_DE.edited.csv', index_col='id')
 del cpp['Unnamed: 0']
 
-print(cpp.columns)
+cpp['max_in'] = cpp['capacity_net_bnetza'] / cpp['efficiency_estimate']
 
-cpp_by_region_fuel = cpp.groupby(['region', 'fuel'])
+cpp_by_region_fuel = cpp.groupby(['region', 'state', 'fuel'])
 
 capacity_by_region_fuel = cpp_by_region_fuel[['capacity_net_bnetza',
-                                              'capacity_gross_uba']].sum()
+                                              'capacity_gross_uba',
+                                              'max_in']].sum()
 
-print(capacity_by_region_fuel)
-print(cpp_by_region_fuel[['efficiency_data', 'efficiency_estimate']].mean())
+capacity_by_region_fuel['eff_new'] = (
+    capacity_by_region_fuel.capacity_net_bnetza /
+    capacity_by_region_fuel.max_in)
+# print(capacity_by_region_fuel.index)
+# capacity_by_region_fuel.set_index('region')
+
+
+print(capacity_by_region_fuel.loc['DE15', ['capacity_net_bnetza', 'eff_new']])
+# print(cpp_by_region_fuel[['efficiency_data', 'efficiency_estimate']].mean())
