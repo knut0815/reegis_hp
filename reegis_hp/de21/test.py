db4bc526a3abda6b8fd75f833dfc8b52fc3ff4a5
uwe <uwe.krien@rl-institut.de>
2017-02-27 21:49:36 +0100

    change tests

--- reegis_hp/de21/test.py
+++ reegis_hp/de21/test.py
@@ -1,10 +1,27 @@
 import pandas as pd
 
 seq_file = 'scenarios/reegis_de_21_test_neu_seq.csv'
-seq_neu = 'scenarios/reegis_de_21_test_neu_neu_seq.csv'
+# seq_neu = 'scenarios/reegis_de_21_test_neu_neu_seq.csv'
+# para_file = 'scenarios/reegis_de_3_test.csv'
+para_file = 'scenarios/EK_test3_neu2.csv'
+seq_neu = 'scenarios/EK_test3_neu2_seq.csv'
 
-tmp_csv = pd.read_csv(seq_file, header=[0, 1, 2, 3, 4],
-                      parse_dates=True, index_col=0)
+df_seq = pd.read_csv(seq_neu, header=[0, 1, 2, 3, 4],
+                     parse_dates=True, index_col=0)
 
-tmp_csv.to_csv(seq_neu)
-print(tmp_csv.index)
+# tmp_csv.to_csv(seq_neu)
+# print(tmp_csv.index)
+
+df = pd.read_csv(para_file, index_col=[0, 1, 2])
+
+mask = df['actual_value'].str.contains('seq').fillna(False)
+a = df[mask].index.tolist()
+print(a[0])
+# print(pd.Series([1, 2, 4]))
+# # df.loc[[a[0]], 'actual_value'] = pd.Series([1, 2, 4])
+# print(df['actual_value'].loc[[a[0]]])
+# s = df.to_dict()
+# print(s['actual_value'][a[0]])
+
+print(df_seq[a[0]])
+
